fc7f4bbfe3e37c4353206a71041360cb7e4029b4
Ramb Memburg <ramb.memburg@gmail.com>
2022-11-03 19:25:22 -0600

    astro conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://memburg.github.io'
+    site: 'https://memburg.github.io/league-of-troll/'
 });
